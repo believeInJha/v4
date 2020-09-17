6590bb4bafbf9cb20832ae3b87a61b03a2837770
Apurv Jha <apurv.bmsit@gmail.com>
2020-09-17 18:39:52 +0530

    fix: hero

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -49,11 +49,11 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Apurv Jha</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const three = <h3 className="big-heading">I build things for the Internet.</h3>;
   const four = (
     <p>
-      I'm a software engineer based in Bangalore, MA specializing in building (and occasionally
-      designing) exceptional websites, applications, and everything in between.
+      I'm a software engineer based in Bangalore, India specializing in building (and occasionally
+      designing) exceptional mobile applications and everything in between.
     </p>
   );
   const five = (
