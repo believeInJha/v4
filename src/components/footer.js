f8bbe6b57dcddc60ba99c24b6e1e687abff28fea
Apurv Jha <apurv.bmsit@gmail.com>
2020-09-17 18:35:58 +0530

    fix: index.md

--- src/components/footer.js
+++ src/components/footer.js
@@ -105,8 +105,8 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
-          <div>Designed &amp; Built by Brittany Chiang</div>
+        <a href="https://github.com/believeInJha/v4">
+          <div>Designed &amp; Built by Apurv Jha</div>
 
           {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
